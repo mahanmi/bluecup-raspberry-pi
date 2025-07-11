9bf5e855aefec6dcc8de7469be96d5bee40103cd
Amir H. Zarei <AmirRivar@gmail.com>
2025-07-12 00:34:36 +0330

    fix: boot_time is not initiated

--- utils/time_handlers.py
+++ utils/time_handlers.py
@@ -1,15 +1,11 @@
 import time
 
-boot_time = 0
-
-
-def set_boot_time():
-    global boot_time
-    boot_time = time.time()
+boot_time = time.time()
 
 
 def time_boot_handler():
     return int((time.time() - boot_time) * 1000)
+
 
 def time_usec_handler():
     return 1
